2ba8eb61daaed36ef92c66651043a95d1d9c5c32
KingBlackCow <sgs1159@naver.com>
2022-02-06 23:59:40 +0900

    feat: security 설정 추가

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당파일로 시큐리티를 활성화
+@Configuration // IOC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //super 삭제 - 기존 시큐리티가 가지고 있는 기능 비활성화
+        http.authorizeRequests()
+                .antMatchers(
+                        "/",
+                        "/user/**",
+                        "/image/**",
+                        "/subscribe/**",
+                        "/comment/**")
+                .authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin") //접근권한이 없으면 여기에서 로그인해라
+                .defaultSuccessUrl("/"); //로그인 완료되면  "/" 로 보내라
+    }
+}

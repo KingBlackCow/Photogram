7db4666c40ca1d53f01326085e664d33dc93c34b
KingBlackCow <sgs1159@naver.com>
2022-02-10 00:49:10 +0900

    feat: 스프링 시큐리티를 이용한 로그인 구현

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -1,17 +1,29 @@
 package com.cos.photogramstart.config.auth;
 
+import com.cos.photogramstart.domain.User.User;
+import com.cos.photogramstart.domain.User.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service //IOC
+@RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
 
+    // 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다.
+    // 2. 리턴이 잘돼면 자동으로 UserDetails 타입을 세션에 저장됨
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("나실행돼? "+ username);
-        return null;
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity ==null){
+            return null;
+        }else{
+            //세션에 저장
+            return new PrincipalDetails(userEntity);
+        }
     }
 }
--- src/main/java/com/cos/photogramstart/domain/User/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/User/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //어노테이션이 없어도 JpaRepository 상속하면 Ioㅊ등록이 자동으로됨
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    //JPA query method
+    User findByUsername(String username);
 }

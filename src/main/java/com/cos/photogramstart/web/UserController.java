6fe1d4c5aaab8ffb3ebbf18c1790cf8fadc7541d
KingBlackCow <sgs1159@naver.com>
2022-02-10 01:30:42 +0900

    feat: 세션정보 알아내기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,10 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.User.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +19,13 @@ public class UserController {
 
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        //AuthenticationPrincipal 세션정보 쉽게얻는방법
+        System.out.println("세션정보:"+ principalDetails.getUser());
+        // 세션정보 어렵게 얻는 방법
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+        System.out.println("직접찾은 세션정보: "+mPrincipalDetails.getUser());
         return "user/update";
     }
 }

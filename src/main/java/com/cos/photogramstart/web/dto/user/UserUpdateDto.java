a100aa1220569162abe10cdb390074f1084fb7ae
KingBlackCow <sgs1159@naver.com>
2022-02-12 02:41:52 +0900

    feat: 회원정보 수정완료

--- src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
@@ -5,8 +5,8 @@ import lombok.Data;
 
 @Data
 public class UserUpdateDto {
-    private String name;
-    private String password;
+    private String name;//필수
+    private String password;//필수
     private String website;
     private String bio;
     private String phone;
@@ -15,8 +15,8 @@ public class UserUpdateDto {
     //조금 위험함. 코드 수정 필요할 예정
     public User toEntity(){
         return User.builder()
-                .name(name)
-                .password(password)
+                .name(name)//이름을 기재 안했으면 문제! Validation체크
+                .password(password) //패스워드를 기재안했으면 문제!! Validation체크
                 .website(website)
                 .bio(bio)
                 .phone(phone)
--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,15 +1,17 @@
 // (1) 회원정보 수정
 function update(userid, event) {
+    event.preventDefault();
     let data = $("#profileUpdate").serialize();
 
     $.ajax({
-        type:"put",
-        url:"api/user/${userid}",
+        type: "put",
+        url: `/api/user/${userid}`,
         data: data,
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType:"json"
     }).done(res=>{
         console.log("성공");
+        location.href=`/user/${userid}`;
     }).fail(error=>{
         console.log("실패");
     })

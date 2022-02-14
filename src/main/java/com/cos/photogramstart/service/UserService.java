ddeb107066f30dd1709c0f83e25ca0a5f255a81e
KingBlackCow <sgs1159@naver.com>
2022-02-14 22:46:25 +0900

    feat: dto로 페이지 완성하기

--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -4,6 +4,7 @@ import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,18 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User 회원프로필(int userid) {
+    public UserProfileDto 회원프로필(int pageUserId, int principalId) {
+        UserProfileDto dto = new UserProfileDto();
+
         // SELECT * FROM image WHERE userid = :userid;
-        User userEntity = userRepository.findById(userid).orElseThrow(() -> {
+        User userEntity = userRepository.findById(pageUserId).orElseThrow(() -> {
             throw new CustomException("해당 프로필 페이지는 없는 페이지입니다.");
         });
-        return userEntity;
+
+        dto.setUser(userEntity);
+        dto.setPageOwnerState(pageUserId == principalId);
+        dto.setImageCount(userEntity.getImages().size());
+        return dto;
     }
 
     @Transactional

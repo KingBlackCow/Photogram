05bb84272adb1e26560d1b87fca1f4e81f948a70
KingBlackCow <sgs1159@naver.com>
2022-02-19 03:58:29 +0900

    좋아요 카운트 뷰 렌더링

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -44,9 +44,9 @@ public class Image { // N,   1
 	
 	@Transient // DB에 칼럼이 만들어지지 않는다.
 	private boolean likeState;
-//
-//	@Transient
-//	private int likeCount;
+
+	@Transient
+	private int likeCount;
 	
 	private LocalDateTime createDate;
 	

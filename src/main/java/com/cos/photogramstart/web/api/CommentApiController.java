661bd697958d068069283dbcf52226d47e5eb2a3
KingBlackCow <sgs1159@naver.com>
2022-02-20 16:04:15 +0900

    feat: 댓글쓰기 완료

--- src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
@@ -4,7 +4,7 @@ import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.comment.Comment;
 import com.cos.photogramstart.service.CommentService;
 import com.cos.photogramstart.web.dto.CMRespDto;
-//import com.cos.photogramstart.web.dto.comment.CommentDto;
+import com.cos.photogramstart.web.dto.comment.CommentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +20,12 @@ public class CommentApiController {
 
 	private final CommentService commentService;
 	
-//	@PostMapping("/api/comment")
-//	public ResponseEntity<?> commentSave(@Valid @RequestBody CommentDto commentDto, BindingResult bindingResult, @AuthenticationPrincipal PrincipalDetails principalDetails){
-//		Comment comment =  commentService.댓글쓰기(commentDto.getContent(), commentDto.getImageId(), principalDetails.getUser().getId()); // content, imageId, userId
-//		return new ResponseEntity<>(new CMRespDto<>(1, "댓글쓰기성공", comment), HttpStatus.CREATED);
-//	}
-	
+	@PostMapping("/api/comment")
+	public ResponseEntity<?> commentSave(@Valid @RequestBody CommentDto commentDto, BindingResult bindingResult, @AuthenticationPrincipal PrincipalDetails principalDetails){
+		Comment comment =  commentService.댓글쓰기(commentDto.getContent(), commentDto.getImageId(), principalDetails.getUser().getId()); // content, imageId, userId
+		return new ResponseEntity<>(new CMRespDto<>(1, "댓글쓰기성공", comment), HttpStatus.CREATED);
+	}
+
 	@DeleteMapping("/api/comment/{id}")
 	public ResponseEntity<?> commentDelete(@PathVariable int id){
 		commentService.댓글삭제(id);

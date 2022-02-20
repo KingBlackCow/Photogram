20205f439bb9de50b73b154a52ba4a98c2d2bc06
KingBlackCow <sgs1159@naver.com>
2022-02-21 01:24:04 +0900

    feat: aop적용

---
+++ src/main/java/com/cos/photogramstart/handler/aop/ValidationAdvice.java
@@ -0,0 +1,37 @@
+package com.cos.photogramstart.handler.aop;
+
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component // RestController, Service 모든 것들이 Component를 상속해서 만들어져 있음.
+@Aspect //aop처리할 수 있는 핸들러가됨
+public class ValidationAdvice {
+
+	//함수 직전(@Before)과 후(after)에도 사용하고 싶을경우 @Around로 쓴다.
+	@Around("execution(* com.cos.photogramstart.web.api.*Controller.*(..))")//*(..) //모든 메서드안에 모든인자
+	public Object apiAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		// proceedingJoinPoint => profile 함수의 모든 곳에 접근할 수 있는 변수
+		// profile 함수보다 먼저 실행
+		//System.out.println("web api 컨트롤러 =====================");
+
+
+
+		return proceedingJoinPoint.proceed(); // profile 함수가 실행됨.
+	}
+
+	@Around("execution(* com.cos.photogramstart.web.*Controller.*(..))")
+	public Object advice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+		//System.out.println("web 컨트롤러 ==========================");
+
+	}
+}

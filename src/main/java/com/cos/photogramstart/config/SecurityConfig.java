7b17013c409d32b63ffc018eee4d952169396e22
KingBlackCow <sgs1159@naver.com>
2022-02-23 01:06:39 +0900

    feat: 회원정보 받기

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.config;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @EnableWebSecurity //해당파일로 시큐리티를 활성화
 @Configuration // IOC
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,6 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder encode(){
         return new BCryptPasswordEncoder();
     }
+
+    private final OAuth2DetailsService oAuth2DetailsService;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -34,6 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/auth/signin") //접근권한이 없으면 여기에서 로그인해라
                 .loginProcessingUrl("/auth/signin")//POST -> 스프링 시큐리티가 로그인 프로세스 진행
-                .defaultSuccessUrl("/"); //로그인 완료되면  "/" 로 보내라
+                .defaultSuccessUrl("/") //로그인 완료되면  "/" 로 보내라
+                .and()
+                .oauth2Login() // form로그인도 하는데, oauth2로그인도 할꺼야!!
+                .userInfoEndpoint() // oauth2로그인을 하면 최종응답을 회원정보를 바로 받을 수 있다.
+                .userService(oAuth2DetailsService);
+
     }
 }
